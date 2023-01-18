72f5dd31d235e3e7536f297bd299581d5d717ecb
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-01-18 17:28:29 -0500

    restart

--- shader.bat
+++
@@ -1,2 +0,0 @@
-glslc res/shader/vulkan.vert -o res/shader/vert.spv
-glslc res/shader/vulkan.frag -o res/shader/frag.spv
--- src/defines.cpp
+++
@@ -1,8 +0,0 @@
-
-#define VMA_IMPLEMENTATION
-#define VMA_STATIC_VULKAN_FUNCTIONS  0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
-#include "vma.hpp"
-
-#define TINYOBJLOADER_IMPLEMENTATION
-#include "tol.hpp"
