81b91ff090119e2e3762cd70d3cf4ac78d009360
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-01-19 15:58:34 -0500

    reder pass ready

--- main.cpp
+++ main.cpp
@@ -39,13 +39,14 @@ int main(int argc, char** argv)
                                                                    aspects);
 
     VkAttachmentDescription attachment_descriptions[5]{};
-    VkAttachmentReference attachment_references0[4]{};
-    VkAttachmentReference attachment_references1[5]{};
+    VkAttachmentReference attachment_references0[1]{};
+    VkAttachmentReference attachment_references1[4]{};
+    VkAttachmentReference attachment_references2[4]{};
 
     for (int i = 0; i < 5; i++)
     {
         attachment_descriptions[i].samples = VK_SAMPLE_COUNT_1_BIT;
-        attachment_descriptions[i].format = VK_FORMAT_R16G16B16_SFLOAT;
+        attachment_descriptions[i].format = VK_FORMAT_R16G16B16A16_SFLOAT;
         attachment_descriptions[i].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
         attachment_descriptions[i].finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
         attachment_descriptions[i].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
@@ -59,12 +60,71 @@ int main(int argc, char** argv)
         }
         if (i == 4)
         {
-
             attachment_descriptions[i].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
             attachment_descriptions[i].format = swapchain.image_format_;
             attachment_descriptions[i].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
         }
     }
+
+    attachment_references0[0].attachment = 3;
+    attachment_references0[0].layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+    for (int i = 0; i < 4; i++)
+    {
+        attachment_references1[i].attachment = i;
+        attachment_references1[i].layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+    }
+    attachment_references1[3].layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+
+    for (int i = 0; i < 4; i++)
+    {
+        attachment_references2[i].attachment = i;
+        attachment_references2[i].layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    }
+    attachment_references2[3].attachment = 4;
+    attachment_references2[3].layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpasses[3]{};
+    subpasses[0].pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpasses[0].pDepthStencilAttachment = attachment_references0;
+
+    subpasses[1].pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpasses[1].colorAttachmentCount = 3;
+    subpasses[1].pColorAttachments = attachment_references1;
+    subpasses[1].pDepthStencilAttachment = attachment_references1 + 3;
+
+    subpasses[2].pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpasses[2].inputAttachmentCount = 3;
+    subpasses[2].pInputAttachments = attachment_references2;
+    subpasses[2].colorAttachmentCount = 1;
+    subpasses[2].pColorAttachments = attachment_references2 + 3;
+
+    VkSubpassDependency dependencies[2]{};
+    dependencies[0].srcSubpass = 0;
+    dependencies[0].dstSubpass = 1;
+    dependencies[0].srcStageMask = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+    dependencies[0].dstStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+    dependencies[0].srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+    dependencies[0].dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
+
+    dependencies[1].srcSubpass = 1;
+    dependencies[1].dstSubpass = 2;
+    dependencies[1].srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    dependencies[1].dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    dependencies[1].srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    dependencies[1].dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT;
+
+    VkRenderPassCreateInfo render_pass_info{};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_info.subpassCount = 3;
+    render_pass_info.pSubpasses = subpasses;
+    render_pass_info.attachmentCount = 5;
+    render_pass_info.pAttachments = attachment_descriptions;
+    render_pass_info.dependencyCount = 2;
+    render_pass_info.pDependencies = dependencies;
+
+    VkRenderPass render_pass;
+    vkCreateRenderPass(device_layer, &render_pass_info, nullptr, &render_pass);
 
     while (!glfwWindowShouldClose(base_layer))
     {
@@ -77,6 +137,7 @@ int main(int argc, char** argv)
         vmaDestroyImage(device_layer, attachment, attachment);
     }
 
+    vkDestroyRenderPass(device_layer, render_pass, nullptr);
     swapchain.destroy(device_layer);
     device_layer.destroy();
     base_layer.destroy();
