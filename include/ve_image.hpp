c6d291e29eb6bf8a6af5e2eaaeec2c621118f0d7
Felix Xing <101950667+felixxing@users.noreply.github.com>
2023-02-06 12:16:48 -0500

    working on image now

--- include/ve_image.hpp
+++ include/ve_image.hpp
@@ -6,7 +6,7 @@
 
 using VeImageBase = MultiType<VkImage, VkDeviceMemory, VmaAllocation, VkImageView>;
 
-struct ImagePixels
+struct VeImagePixels
 {
     int width = 0;
     int height = 0;
@@ -16,6 +16,12 @@ struct ImagePixels
     stbi_uc* pixels = nullptr;
 };
 
-ImagePixels load_pixel(const std::string file_name, uint32_t channel);
+VeImagePixels load_pixel(const std::string file_name, uint32_t channel);
+void free_pixel(VeImagePixels& pixel_data);
+
+struct VePixelImageBase : public VeImageBase
+{
+    VeImagePixels pixels_data;
+};
 
 #endif // VE_IMAGE_HPP
--- src/ve_image.cpp
+++ src/ve_image.cpp
@@ -3,17 +3,23 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
-ImagePixels load_pixel(const std::string file_name, uint32_t channel)
+VeImagePixels load_pixel(const std::string file_name, uint32_t channel)
 {
-    ImagePixels image_pixels{};
+    VeImagePixels image_pixels{};
 
     image_pixels.pixels =
         stbi_load("res/texture.jpg", &image_pixels.width, &image_pixels.height, &image_pixels.channel, channel);
     if (image_pixels.pixels == nullptr)
     {
-        return ImagePixels();
+        return VeImagePixels();
     }
     image_pixels.size = image_pixels.width * image_pixels.height * channel;
 
     return image_pixels;
+}
+
+void free_pixel(VeImagePixels& pixel_data)
+{
+    stbi_image_free(pixel_data.pixels);
+    pixel_data = VeImagePixels();
 }
